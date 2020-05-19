e679bdd39beacc8088a2f5f25c6d3ad99b001632
Hristo Manov <45336093+hmanov@users.noreply.github.com>
2020-05-19 14:31:41 +0300

    Update profileActions.js

--- client/src/reducers/profileActions.js
+++ client/src/reducers/profileActions.js
@@ -17,3 +17,29 @@ export const getCurrentProfile = (id) => async (dispatch) => {
     });
   }
 };
+
+export const createProfile = (formData, history, edit = false) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+    const res = await axios.post(url + '/api/profile', formData, config);
+    dispatch({ type: GET_PROFILE, payload: res.data });
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach((err) => dispatch(setAlert(err.msg, 'danger')));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status },
+    });
+  }
+};
