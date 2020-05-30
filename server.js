1c528f40e4fdb8bc5f777fa6d48885cf3aa32234
Hristo Manov <45336093+hmanov@users.noreply.github.com>
2020-05-30 17:51:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 app.use(express.static('client/build/'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT} `));
